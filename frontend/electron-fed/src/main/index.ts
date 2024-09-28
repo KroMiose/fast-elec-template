fe7293ec08f549ed150c47b961d011bb24adb1ff
KroMiose <li_xiangff@163.com>
2024-09-28 15:55:36 +0800

    update

--- frontend/electron-fed/src/main/index.ts
+++ frontend/electron-fed/src/main/index.ts
@@ -2,6 +2,7 @@ import { app, shell, BrowserWindow, ipcMain } from 'electron'
 import { join } from 'path'
 import { electronApp, optimizer, is } from '@electron-toolkit/utils'
 import icon from '../../resources/icon.png?asset'
+import { spawn } from 'child_process'
 
 function createWindow(): void {
   // Create the browser window.
@@ -58,6 +59,13 @@ app.whenReady().then(() => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+
+  const fastapi = spawn('..\\backend\\start.bat')
+
+  app.on('window-all-closed', () => {
+    fastapi.kill()
+    app.quit()
   })
 })
 
